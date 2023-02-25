5bbe138ca002e00d388f94954bed15d85f73a241
Chris Razor <getworkchris@gmail.com>
2023-02-25 08:02:26 -0500

    Typed is in the project

---
+++ index.js
@@ -0,0 +1,9 @@
+// var typed = new Typed('.anim', {
+//     // Waits 1000ms after typing "First"
+//     strings: ['Web Developer', 'Graphic Designer',],
+
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     loop: true,
+
+//   });
